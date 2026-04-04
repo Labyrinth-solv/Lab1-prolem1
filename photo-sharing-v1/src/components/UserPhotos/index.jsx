df596311973a363065a37b3684395fad862a4807
Le Trung Duc <letrungduchp1982@gmail.com>
2026-04-04 12:13:37 +0700

    commit mới

--- photo-sharing-v1/src/components/UserPhotos/index.jsx
+++ photo-sharing-v1/src/components/UserPhotos/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography } from "@mui/material";
 import { useParams, Link } from "react-router-dom";
 
@@ -17,50 +17,67 @@ function UserPhotos() {
       <div>
         <Typography variant="h5">User Photos</Typography>
 
-        {photos.map((photo) => (
-          <div key={photo._id} style={{ marginBottom: "20px" }}>
-            
-         
-            <img
-              src={`/images/${photo.file_name}`}
-              alt=""
-              style={{ width: "300px" }}
-            />
+        {photos.map((photo) => {
+          
+          // 👇 xử lý tên file
+          const baseName = photo.file_name.split(".")[0].toLowerCase();
+          const ext = photo.file_name.split(".")[1];
 
-            
-            <Typography variant="body2">
-              {new Date(photo.date_time).toLocaleString()}
-            </Typography>
+          const candidates = [
+            `${baseName}.${ext}`,
+            `${baseName}1.${ext}`,
+            `${baseName}2.${ext}`,
+            `${baseName}3.${ext}`,
+          ];
 
-            
-            <Typography variant="subtitle1">Comments:</Typography>
+          const [imgIndex, setImgIndex] = useState(0);
 
-            
-            {photo.comments && photo.comments.length > 0 ? (
-              photo.comments.map((cmt) => (
-                <div key={cmt._id} style={{ marginLeft: "20px" }}>
-                  
-                  <Link to={`/users/${cmt.user._id}`}>
-                    {cmt.user.first_name} {cmt.user.last_name}
-                  </Link>
+          return (
+            <div key={photo._id} style={{ marginBottom: "20px" }}>
+              
+              <img
+                src={`/images/${candidates[imgIndex]}`}
+                alt=""
+                style={{ width: "300px" }}
+                onError={() => {
+                  if (imgIndex < candidates.length - 1) {
+                    setImgIndex(imgIndex + 1);
+                  }
+                }}
+              />
 
-                  <Typography variant="body2">
-                    {cmt.comment}
-                  </Typography>
-
-                  <Typography variant="caption">
-                    {new Date(cmt.date_time).toLocaleString()}
-                  </Typography>
-                </div>
-              ))
-            ) : (
-              <Typography variant="body2" style={{ marginLeft: "20px" }}>
-                No comments
+              <Typography variant="body2">
+                {new Date(photo.date_time).toLocaleString()}
               </Typography>
-            )}
 
-          </div>
-        ))}
+              <Typography variant="subtitle1">Comments:</Typography>
+
+              {photo.comments && photo.comments.length > 0 ? (
+                photo.comments.map((cmt) => (
+                  <div key={cmt._id} style={{ marginLeft: "20px" }}>
+                    
+                    <Link to={`/users/${cmt.user._id}`}>
+                      {cmt.user.first_name} {cmt.user.last_name}
+                    </Link>
+
+                    <Typography variant="body2">
+                      {cmt.comment}
+                    </Typography>
+
+                    <Typography variant="caption">
+                      {new Date(cmt.date_time).toLocaleString()}
+                    </Typography>
+                  </div>
+                ))
+              ) : (
+                <Typography variant="body2" style={{ marginLeft: "20px" }}>
+                  No comments
+                </Typography>
+              )}
+
+            </div>
+          );
+        })}
       </div>
     );
 }
